038a5531e483e85c8f83535af919a85e79f8de43
Peter Guntermann <pgu@posteo.de>
2022-09-08 03:17:12 +0200

    Add ag grid

--- script.js
+++ script.js
@@ -6,12 +6,34 @@ const config = {
     }
 };
 
+let rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 }
+];
+let columnDefs = [
+    {field: "make"},
+    {field: "model"},
+    {field: "price"},
+];
+const gridOptions = {
+    columnDefs: columnDefs,
+    rowData: rowData
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const songliste = document.querySelector('#songliste')
+    console.log("gridOptions", gridOptions);
+    new agGrid.Grid(songliste, gridOptions);
+});
+
 fetch(url, config)
     .then(res => res
         .text()
         .then(csvString => {
             const data = mapCsvStringToObjects(csvString)
             appendDataToTable(data)
+            gridOptions.api.setRowData(rowData);
         }))
 
 function mapCsvStringToObjects(csvString) {
@@ -22,11 +44,14 @@ function mapCsvStringToObjects(csvString) {
 }
 
 function appendDataToTable(data) {
-    const songliste = document.getElementById("songliste")
-    data.forEach(row => {
-        const divNode = document.createElement("div")
-        divNode.textContent = row
-        songliste.appendChild(divNode)
-    })
+    // const songliste = document.getElementById("songliste")
+
+    gridOptions.api.setRowData(data)
+
+    // data.forEach(row => {
+    //     const divNode = document.createElement("div")
+    //     divNode.textContent = row
+    //     songliste.appendChild(divNode)
+    // })
 }
 
