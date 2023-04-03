8aa08c683398cc291938862c5cf09a7b61bc6573
Peter Guntermann <peter.guntermann.ext@siemens.com>
2023-04-03 11:45:59 +0200

    Create new ASP.NET MVC Web app

---
+++ CoversongsAspnetMvc/CoversongsAspnetMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CoversongsAspnetMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
