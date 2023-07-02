838eadcc190ed2adc79b13598e247a496ee5788c
Peter Guntermann <pgu@posteo.de>
2023-07-02 21:33:24 +0200

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AgGridModule } from "ag-grid-angular";
+import { ButtonsModule } from "ngx-bootstrap/buttons";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, AgGridModule, HttpClientModule],
+    imports: [BrowserModule, AppRoutingModule, AgGridModule, HttpClientModule, ButtonsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
