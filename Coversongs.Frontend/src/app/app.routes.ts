4f6e569936b13b6b1140897183b4fcf31d979629
Peter Guntermann <pgu@posteo.de>
2024-06-01 12:46:35 +0200

    add playground component

--- Coversongs.Frontend/src/app/app.routes.ts
+++ Coversongs.Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import type { Routes } from '@angular/router';
+import { PlaygroundComponent } from './playground/playground.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'test', component: PlaygroundComponent },
+  { path: '**', redirectTo: 'test' },
+];
