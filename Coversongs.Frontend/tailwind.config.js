850d8590002092afeaadb68580b8ed9c581d8f06
Peter Guntermann <pgu@posteo.de>
2024-06-01 12:57:30 +0200

    install daisyUI
    npm i -D daisyui@latest
    https://daisyui.com/docs/install/

--- Coversongs.Frontend/tailwind.config.js
+++ Coversongs.Frontend/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
