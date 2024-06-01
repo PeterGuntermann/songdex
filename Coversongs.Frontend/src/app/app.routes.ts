22c6b642c5bc2f10c4f5ed607cd497bb82d2d4cc
Peter Guntermann <pgu@posteo.de>
2024-06-01 14:17:23 +0200

    create component for songlist

--- Coversongs.Frontend/src/app/app.routes.ts
+++ Coversongs.Frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import type { Routes } from '@angular/router';
 import { PlaygroundComponent } from './playground/playground.component';
+import { SonglistComponent } from './songlist/songlist.component';
 
 export const routes: Routes = [
   { path: 'test', component: PlaygroundComponent },
-  { path: '**', redirectTo: 'test' },
+  { path: 'songlist', component: SonglistComponent },
+  { path: '**', redirectTo: 'songlist' },
 ];
