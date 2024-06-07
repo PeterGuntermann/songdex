27bfb08e1c67ef3c6d5c813f02df87ea774ef58b
Peter Guntermann <pgu@posteo.de>
2024-06-08 00:23:39 +0200

    add Dockerfile for frontend

--- build-images.ps1
+++ build-images.ps1
@@ -4,6 +4,11 @@ Push-Location ./Coversongs.Backend
 docker build -t coversongs-backend .
 Pop-Location
 
+Write-Host -ForegroundColor DarkMagenta "Build frontend ..."
+Push-Location ./Coversongs.Frontend
+docker build -t coversongs-frontend .
+Pop-Location
+
 docker images
 
 Write-Host -ForegroundColor DarkMagenta "Start containers ..."
