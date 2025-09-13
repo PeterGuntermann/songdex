9e1caf190a5bc174f0e46703e9b177bcb39ddb43
Peter Guntermann <pgu@posteo.de>
2025-09-13 22:02:23 +0200

    move Controller

--- Coversongs.Backend/Coversongs.Backend.WebApi/Endpoints/CoversongsController.cs
+++ Coversongs.Backend/Coversongs.Backend.WebApi/Endpoints/CoversongsController.cs
@@ -1,7 +1,7 @@
 ﻿using Coversongs.Backend.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Coversongs.Backend.Service.Coversongs;
+namespace Coversongs.Backend.WebApi.Endpoints;
 
 [Route("api/[controller]")]
 public class CoversongsController(ICoversongsService coversongsService) : Controller
