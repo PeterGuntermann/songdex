cdf602fc9c45e6fd503ab8b5d2dac77b6b19bb7b
Peter Guntermann <pgu@posteo.de>
2025-09-22 14:02:47 +0200

    extract more WebApi extensions

--- Coversongs.Backend/Coversongs.Backend.Service/Program.cs
+++ Coversongs.Backend/Coversongs.Backend.Service/Program.cs
@@ -7,23 +7,7 @@ var services = builder.Services;
 services.AddWebApi();
 services.AddSingleton<ICoversongsService, CoversongsService>();
 
-
 var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseRouting();
-app.UseCors(WebApiConstants.AllowSpecificOrigins);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseWebApi();
 
 app.Run();
--- Coversongs.Backend/Coversongs.Backend.WebApi/WebApiExtensions.cs
+++ Coversongs.Backend/Coversongs.Backend.WebApi/WebApiExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Coversongs.Backend.WebApi;
 
@@ -18,5 +20,25 @@ public static class WebApiExtensions
         });
         services.AddControllers();
         return services;
+    }
+
+    public static WebApplication UseWebApi(this WebApplication app)
+    {
+        
+        if (!app.Environment.IsDevelopment())
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseRouting();
+        app.UseCors(WebApiConstants.AllowSpecificOrigins);
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller}/{action=Index}/{id?}");
+
+        app.MapGet("/", () => "Hello World!");
+        return app;
     }
 }
