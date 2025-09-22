3e9d648e91b0e32f6b77c04ce8cdb8e5058f1287
Peter Guntermann <pgu@posteo.de>
2025-09-22 15:36:11 +0200

    add Result utility

---
+++ Songdex.Backend/Songdex.Backend.Common/Result.cs
@@ -0,0 +1,20 @@
+﻿namespace Songdex.Backend.Common;
+
+public class Result<T>
+{
+    public T? Value { get; set; }
+    public string? FailureReason { get; set; }
+
+    public static Result<T> Success(T value) => new()
+    {
+        Value = value
+    };
+    
+    public static Result<T> Failure(string reason) => new()
+    {
+        FailureReason = reason
+    };
+
+    public bool IsSuccess() => Value is not null;
+    public bool IsFailure() => FailureReason is not null;
+}
