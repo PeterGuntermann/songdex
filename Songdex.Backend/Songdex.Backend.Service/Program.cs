6289571cb28abf5c41f69c6a33087b81556b7d23
Peter Guntermann <peter.guntermann.ext@siemens.com>
2025-10-10 20:07:59 +0200

    add LocalDatabaseInitializer for db seeding

--- Songdex.Backend/Songdex.Backend.Service/Program.cs
+++ Songdex.Backend/Songdex.Backend.Service/Program.cs
@@ -1,5 +1,6 @@
 using Songdex.Backend.Application;
 using Songdex.Backend.Infrastructure;
+using Songdex.Backend.Service.Seeding;
 using Songdex.Backend.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,11 @@ var services = builder.Services;
 services.AddApplication();
 services.AddInfrastructure();
 services.AddWebApi();
+
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+{
+    services.AddHostedService<LocalDatabaseInitializer>();
+}
 
 var app = builder.Build();
 app.UseWebApi();
---
+++ Songdex.Backend/Songdex.Backend.Service/Seeding/LocalDatabaseInitializer.cs
@@ -0,0 +1,44 @@
+using Songdex.Backend.Domain.Model;
+using Songdex.Backend.Infrastructure.Database;
+
+namespace Songdex.Backend.Service.Seeding;
+
+public class LocalDatabaseInitializer(
+    IServiceScopeFactory serviceScopeFactory,
+    ILogger<LocalDatabaseInitializer> logger)
+    : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        // DEBUG ONLY
+        return;
+        
+        await using var scope = serviceScopeFactory.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<SongdexDbContext>();
+
+        var numberOfSongs = context.Songs.Count();
+        if (numberOfSongs == 0)
+        {
+            logger.LogInformation("Initializing empty local database...");
+
+            var songs = new List<Song>
+            {
+                new() { Id = 42, Title = "Foobar" }
+            };
+            context.Songs.AddRange(songs);
+
+            await context.SaveChangesAsync(cancellationToken);
+        }
+        else
+        {
+            logger.LogInformation(
+                "Found non-empty database with {Count} songs. Skip initializing...",
+                numberOfSongs);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
