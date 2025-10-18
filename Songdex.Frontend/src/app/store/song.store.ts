3311e68c9462afbe7a09405b34d9e0b60c10815a
Peter Guntermann <pgu@posteo.de>
2025-10-18 23:11:30 +0200

    refactor: make SongState more scalable for extensions

--- Songdex.Frontend/src/app/store/song.store.ts
+++ Songdex.Frontend/src/app/store/song.store.ts
@@ -1,12 +1,23 @@
-import { signalStore, withState } from "@ngrx/signals";
-import { newSong, type Song } from "../models/song";
-import { generateDummySongs } from "../util/dummy-data";
+import { inject, InjectionToken } from "@angular/core";
+import { signalStore, withState } from '@ngrx/signals';
+import { newSong, type Song } from '../models/song';
+import { generateDummySongs } from '../util/dummy-data';
+
+export type SongState = {
+  allSongs: Song[];
+};
+
+export const initialState: SongState = {
+  allSongs: [newSong('From signal store'), ...generateDummySongs()],
+};
+
+export const SONG_STATE = new InjectionToken<SongState>('SONG_STATE', {
+  factory: () => initialState,
+});
 
 export const SongStore = signalStore(
   { providedIn: 'root' },
-  withState({
-    allSongs: [newSong('From signal store'), ...generateDummySongs()] as Song[],
-  }),
+  withState(() => inject(SONG_STATE)),
   // withComputed({
   //   songById: (id: string) => this.songs().find((song) => song.id === id);
   // }),
