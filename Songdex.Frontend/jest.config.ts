b364394d538b3f4597caca4c51c74a0779861441
Peter Guntermann <peter.guntermann.ext@siemens.com>
2025-10-18 12:54:55 +0200

    test: add jest configuration

---
+++ Songdex.Frontend/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+import { createCjsPreset } from 'jest-preset-angular/build/presets/create-cjs-preset';
+import * as path from 'path';
+
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+let preset = {
+  ...createCjsPreset(),
+  collectCoverage: false,
+  coverageDirectory: path.join(__dirname, 'dist/coverage/'),
+  coverageReporters: ['html', 'cobertura', 'lcov', 'text-summary'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  transformIgnorePatterns: ['node_modules/(?!@sisuite/sec-common)/'],
+} satisfies Config;
+
+export default preset;
